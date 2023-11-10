a10ba26a451139a7a46e7879b4bf5d4f83d81f67
DuckDoesStuff <mailtienduc@gmail.com>
2023-11-10 13:34:43 +0700

    Routing a bit

--- app.js
+++ app.js
@@ -14,14 +14,20 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
