f544ba5d1fa94ca033d846b7bf54b78383211300
nhungnguyen1503 <nhung1899@gmail.com>
2023-12-02 13:11:34 +0700

    checkout to main

--- routes/admin/product/product-dashboard.js
+++ routes/admin/product/product-dashboard.js
@@ -1,47 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const productController = require('../../../src/product/product.controller');
 
-const product_info = [
-    {
-        id:123,
-        name: "COOL GREEN DRESS WITH RED BELL",
-        price: 47,
-        discount: 7,
-        availability: 30,
-    },
-    {
-        id:123,
-        name: "COOL GREEN DRESS WITH RED BELL",
-        price: 47,
-        discount: 7,
-        availability: 30,
-    },
-    {
-        id:123,
-        name: "COOL GREEN DRESS WITH RED BELL",
-        price: 47,
-        discount: 7,
-        availability: 30,
-    },
-    {
-        id:123,
-        name: "COOL GREEN DRESS WITH RED BELL",
-        price: 47,
-        discount: 7,
-        availability: 30,
-    },
-    {
-        id:123,
-        name: "COOL GREEN DRESS WITH RED BELL",
-        price: 47,
-        discount: 7,
-        availability: 30,
-    }
-]
+const productsPerPage = 2; // Số sản phẩm mỗi trang
 
+function paginateProducts(products, currentPage) {
+  const startIndex = (currentPage - 1) * productsPerPage;
+  const endIndex = startIndex + productsPerPage;
+  return products.slice(startIndex, endIndex);
+}
 
-router.get("/", function (req, res, next) {
-    res.render("admin/product/product-dashboard", {product_info: product_info});
+async function generateProductDashboard() {
+    const allProducts = await productController.getAllProduct();
+    const totalPages = Math.ceil(allProducts.length / productsPerPage);
+    const currentPage = 1; // Đối với trang đầu tiên
+    const paginatedProducts = paginateProducts(allProducts, currentPage);
+    return {
+        allProducts: paginatedProducts,
+        totalPages,
+        currentPage,
+      };
+}
+
+router.get("/", async function (req, res, next) {
+    let currentPage = req.query.page || 1;
+    currentPage = parseInt(currentPage, 2);
+
+    const data = await generateProductDashboard(currentPage);
+    res.render("admin/product/product-dashboard", data);
 });
 
 module.exports = router;
